696702d0f9b0d8c5c1c132299851490ce7e5c0c7
lomiu1 <teazybearfresauwu@gmail.com>
2025-11-14 14:16:39 +0000

    Configura base para rutas correctas en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://lomiu1.github.io',
-  base: '/lomiu1.github.io/gi',
+  base: '/Proyecto.github.io/',
 })
